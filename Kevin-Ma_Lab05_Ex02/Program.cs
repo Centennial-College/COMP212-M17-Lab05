2d6bae25b8b24492a5997e98f127b9dd0eea700f
Kevin Ma <kbma.kevin@gmail.com>
2017-07-23 02:03:08 -0400

    implemented exercise2

--- Kevin-Ma_Lab05_Ex02/Program.cs
+++ Kevin-Ma_Lab05_Ex02/Program.cs
@@ -30,8 +30,67 @@ namespace Kevin_Ma_Lab05_Ex02
             DisplayArray(doubleArray); // pass a double array argument
             Console.Write("Array charArray contains: ");
             DisplayArray(charArray); // pass a char array argument
-        }
 
+            Console.WriteLine("\nOverloaded method demo below...\n");
+
+            //NEW CODE I ADDED+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+            Console.WriteLine("VALID Inputs");
+            Console.WriteLine("------------------------------------------------------------------------------------------");
+            //tested middle section
+            Console.Write("Displaying intArray from index 2 to 5 (non-inclusive): ");
+            Console.WriteLine($"{DisplayArray(intArray, 2, 5)} elements were displayed"); // pass an int array argument to the overloaded method
+            Console.WriteLine();
+
+            //tested starting boundary
+            Console.Write("Displaying doubleArray from index 0 to 4 (non-inclusive): ");
+            Console.WriteLine($"{DisplayArray(doubleArray, 0, 4)} elements were displayed"); // pass a double array argument to the overloaded method
+            Console.WriteLine();
+
+            //tested end boundary
+            Console.Write("Displaying charArray from index 3 to charArray.Length (non-inclusive): ");
+            Console.WriteLine($"{DisplayArray(charArray, 3, charArray.Length)} elements were displayed"); // pass a char array argument to the overloaded method
+            Console.WriteLine();
+
+            //INVALID Inputs
+            Console.WriteLine("INVALID Inputs");
+            Console.WriteLine("------------------------------------------------------------------------------------------");
+            try
+            {
+                //lowIndex out of bounds
+                Console.Write("Displaying intArray from index -2 to 5 (non-inclusive): ");
+                Console.WriteLine($"{DisplayArray(intArray, -2, 5)} elements were displayed"); // pass an int array argument to the                 overloaded method
+
+            }
+            catch (ArgumentException e)
+            {
+                Console.WriteLine("\n" + e.Message);
+            }
+            Console.WriteLine();
+
+            try
+            {
+                //highIndex out of bounds
+                Console.Write("Displaying doubleArray from index 3 to 50 (non-inclusive): ");
+                Console.WriteLine($"{DisplayArray(doubleArray, 3, 50)} elements were displayed"); // pass a double array argument to the                 overloaded method
+            }
+            catch (ArgumentException e)
+            {
+                Console.WriteLine("\n" + e.Message);
+            }
+            Console.WriteLine();
+
+            try
+            {
+                //highIndex <= lowIndex
+                Console.Write("Displaying charArray from index 4 to 3 (non-inclusive): ");
+                Console.WriteLine($"{DisplayArray(charArray, 4, 3)} elements were displayed"); // pass a char array argument to the                 overloaded method
+            }
+            catch (ArgumentException e)
+            {
+                Console.WriteLine("\n" + e.Message);
+            }
+
+        }
         //FROM THE CLASS EXAMPLE+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
         // output array of all types                           
         private static void DisplayArray<T>(T[] inputArray)
@@ -42,6 +101,42 @@ namespace Kevin_Ma_Lab05_Ex02
             }
 
             Console.WriteLine();
+        }
+
+        //NEW CODE I ADDED+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+        /// <summary>
+        /// This method displays only the designated portion of the array (starting at lowIndex, up to but non-inclusive of highIndex)
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="inputArray"></param>
+        /// <param name="lowIndex"></param>
+        /// <param name="highIndex"></param>
+        /// <returns>
+        ///     the number of elements displayed
+        /// </returns>
+        private static int DisplayArray<T>(T[] inputArray, int lowIndex, int highIndex)
+        {
+            //validate lowIndex and highIndex
+            //if either is out of range,
+            //or high is <= low, throw ArgumentException
+            if (
+                    lowIndex < 0 || lowIndex > inputArray.Length ||
+                    highIndex < 0 || highIndex > inputArray.Length ||
+                    highIndex <= lowIndex
+                )
+            {
+                throw new ArgumentException("***Invalid arguments - either one of the indices are out of bounds or highIndex is less than or equal to lowIndex.");
+            }
+
+            //valid input, so display designated portion of the array
+            for (int i = lowIndex; i < highIndex; i++)
+            {
+                Console.Write($"{inputArray[i]} ");
+            }
+            Console.WriteLine();
+
+            //return the number of elements displayed
+            return highIndex - lowIndex;
         }
     }
 }
